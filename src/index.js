37fb57c53e494017109ea2f6a2ea4560bc92b053
Ofoegbu Goodnews <goodnews@softcom.ng>
2019-08-21 22:47:43 +0100

    Latest update add staging url CORS

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import server from './server'
 
 const cors = {
     credentials: true,
-    origin: "http://localhost:3000"
+    origin: ["http://localhost:3000", "https://react-apollo-boilerplate.herokuapp.com"]
 }
 
 server.start({ cors, port: process.env.PORT || 4000 }, () => {
